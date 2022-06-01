963f5e705b34afaae978db30834221afe24ee7b7
JuergenSchneider25 <juergenschneider2512@gmail.com>
2022-06-01 14:43:19 +0200

    ProjectMiniTwitterDone

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -1,7 +1,36 @@
-@extends('layout/master') 
+@extends('layout/master')
+
 
 @section('title', 'Mini Twitter')
 
-@section ('content')
+
+@section('content')
+
+
+<h2>Create new message: </h2>
+
+<form action="/create" method="post">
+    <input type="text" name="title" placeholder="Title">
+    <input type="text" name="content" placeholder="Content">
+
+    @csrf
+    <button type="submit">Send</button>
+
+</form>
+
 <h2>Recent messages:</h2>
+
+<ul>
+
+    @foreach ($messages as $message)
+    <li>
+        <b>
+
+            <a href="/message/{{$message->id}}">{{$message->title}}:</b><br>
+        {{$message->content}}<br>
+        {{$message->created_at->diffForHumans()}}
+    </li>
+    @endforeach
+</ul>
+
 @endsection
